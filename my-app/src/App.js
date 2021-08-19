c2581b796abd95d14bf6fe829e497717226f84a4
Om Gaikwad <62072438+omgaikwad@users.noreply.github.com>
2021-08-19 16:48:29 +0530

    add app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+var emojiDictionary = {
+  "😂": "laughing",
+  "😭": "crying",
+  "👌": "ok hand",
+  "🤔": "thinking",
+  "🙄": "face with rolling eyes",
+  "😌": "relieved",
+  "😩": "weary",
+  "💯": "hundred points",
+  "😜": "winking face with tounge",
+  "🙌": "raising Hands",
+  "👀": "eyes",
+  "🔥": "fire",
+  "😄": "grinning Face with smiling eyes",
+  "🙊": "speak no evil monkey",
+  "😕": "confused",
+  "😝": "squinting with tounge",
+  "🌸": "cherry blossom",
+  "😞": "tired",
+  "😃": "grinning",
+  "🤦": "person facepalming"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    setMeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>emoji interpreter</h1>
+      <span style={{ fontSize: "small" }}>
+        developed using ReactJS by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
+          href="https://twitter.com/OmGaikwad_"
         >
-          Learn React
+          {" "}
+          om{" "}
         </a>
-      </header>
+      </span>
+      <input onChange={emojiInputHandler}></input>
+      <h2> {meaning} </h2>
+
+      <h3>emoji's we know</h3>
+      {emojisWeKnow.map((emoji) => {
+        return (
+          <span key={emoji} onClick={() => emojiClickHandler(emoji)}>
+            {" "}
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
